f8ecca11dd5770844844a32fb2ae2b15693ea69d
Isal0192 <faisalfajar1305@gmail.com>
2025-04-15 18:31:56 +0700

    first commit

--- app/src/main/java/com/example/jagajajan/MainActivity.java
+++ app/src/main/java/com/example/jagajajan/MainActivity.java
@@ -1,13 +1,25 @@
 package com.example.jagajajan;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
-
+    Button get_started;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        get_started = findViewById(R.id.get_stared);
+
+        get_started.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent to_get_started = new Intent(getApplicationContext(), getStarted.class);
+                startActivity(to_get_started);
+            }
+        });
     }
 }
